c6b214d464857af7e95df943bb64d61c66f0538c
LSVDnepr <litovchenkosv1990@gmail.com>
2018-06-08 03:11:23 +0300

    Viper iOS

--- ChatAppTdd.iOS/AuthModule/AuthRouter.cs
+++ ChatAppTdd.iOS/AuthModule/AuthRouter.cs
@@ -1,27 +1,32 @@
 ﻿using System;
 using ChatAppTdd.AuthModule;
 using UIKit;
+using Foundation;
 
 
 namespace ChatAppTdd.iOS.AuthModule
 {
-	public class AuthRouter : IAuthRouter
+    public class AuthRouter : IAuthRouter
     {
-		private readonly UINavigationController _navController;
+        private readonly UINavigationController _navController;
 
-		public AuthRouter(UINavigationController navigationController)
+        public AuthRouter(UINavigationController navigationController)
         {
-			_navController = navigationController ?? throw new ArgumentNullException("Provided navigation controller is null!Recheck"); 
+            _navController = navigationController ?? throw new ArgumentNullException("Provided navigation controller is null! Recheck"); 
         }
 
-		public void MoveToChat(string sessionId, string userId)
-		{
-			throw new NotImplementedException();
-		}
+        public void MoveToChat(string sessionId, string userId)
+        {
+            var destVC = UIStoryboard.FromName("Main", NSBundle.MainBundle).InstantiateViewController("ChatPageVC") as ChatPageVC;
+            destVC.SessionId = sessionId ?? throw new ArgumentNullException("Provided sessionId is null!Recheck!");
+            destVC.UserId = userId ?? throw new ArgumentNullException("Provided userId is null!Recheck!");
+            _navController.PushViewController(destVC, true);
+        }
 
-		public void MoveToRegistration()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public void MoveToRegistration()
+        {
+            var destVC = UIStoryboard.FromName("Main", NSBundle.MainBundle).InstantiateViewController("RegisterVC")as RegisterVC;
+            _navController.PushViewController(destVC, true);
+        }
+    }
 }
--- ChatAppTdd.iOS/AuthorizeVC.designer.cs
+++ ChatAppTdd.iOS/AuthorizeVC.designer.cs
@@ -14,8 +14,16 @@ namespace ChatAppTdd.iOS
     [Register ("AuthorizeVC")]
     partial class AuthorizeVC
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        ChatAppTdd.iOS.AuthPageView _authPageView { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (_authPageView != null) {
+                _authPageView.Dispose ();
+                _authPageView = null;
+            }
         }
     }
 }
